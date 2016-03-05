f14e202fa097197c42c7e396f34be29e7a224626
Kush Mansingh <kushmansingh@icloud.com>
2016-03-05 16:28:12 -0500

    Remove hardcoded business enum options

--- kabbage/views.py
+++ kabbage/views.py
@@ -2,9 +2,40 @@ from flask import jsonify, request, render_template
 
 from kabbage import app
 
+BUSINESS_TYPES = [
+    'Accounting',
+    'Amusement',
+    'AutoRepair',
+    'BusinessServices',
+    'Catering',
+    'ChildCare',
+    'ComputerServices',
+    'ConsumerGoodsRetailStore',
+    'ConsumerGoodsOnlineStore',
+    'ConsumerGoodsOnlineAndOffline',
+    'Construction',
+    'Dentists',
+    'DryCleaning',
+    'Equipment',
+    'FoodService',
+    'Grocery',
+    'Health',
+    'HomeRepair',
+    'Hotels',
+    'Insurance',
+    'Janitorial',
+    'Landscape',
+    'Optometrists',
+    'Physicians',
+    'Restaurants',
+    'Salons',
+    'Taxis',
+    'Trucking',
+    'Veterinarians']
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     error = None
     if request.method == 'GET':
-        return render_template('home.html', error=error)
+        return render_template('home.html', error=error, businessTypes=BUSINESS_TYPES)
 
